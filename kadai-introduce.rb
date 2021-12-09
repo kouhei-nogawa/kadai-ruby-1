2ccb5e997b0c20a170fa09f5c9a7295efd1bc922
kouhei.nogawa <nk04084163@gmail.com>
2021-12-09 03:36:49 +0000

    kadai-ruby-1

---
+++ kadai-introduce.rb
@@ -0,0 +1,18 @@
+myouji = '野川'
+namae = '皓平'
+
+fullname = myouji + namae 
+
+nenrei = 25
+
+desu = 'です。'
+namaesyoukai = fullname + desu 
+
+saidesu ='歳です。'
+ nenreisyoukai = nenrei.to_s + saidesu
+ 
+ 
+ jikosyoukai = namaesyoukai + nenreisyoukai
+ puts jikosyoukai
+ 
+
